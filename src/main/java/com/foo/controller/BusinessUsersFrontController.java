405fdf68035ca002f08f288ca226c4faaa4f165f
leesr94 <leesr94 sr940718@gmail.com>
2023-07-26 21:31:52 +0900

    Feat : business 컨트롤러 생성 - BusinessUserFrontController 생성 - BusinessUserRestTest 생성 - view/business/join.jsp 생성

---
+++ src/main/java/com/foo/controller/BusinessUsersFrontController.java
@@ -0,0 +1,27 @@
+package com.foo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/business")
+public class BusinessUsersFrontController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BusinessUsersFrontController.class);
+	
+	
+	
+	// http://localhost:8088/business/join
+	// 사업자 회원가입
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String businessUsersJoin(Model model) {
+		logger.debug("사업자 회원가입 - businessUsersJoin() 실행");
+		
+		return "/business/join";
+	}
+	
+}
