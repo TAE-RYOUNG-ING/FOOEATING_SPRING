0ccf761215d581273c801ee7b0f49a5677320ae8
LingLing <ryoung6920@gmail.com>
2023-07-31 00:38:39 +0900

    Feat : main페이지의 로그인아웃 버튼 제어

--- src/main/java/com/foo/controller/UserFrontController.java
+++ src/main/java/com/foo/controller/UserFrontController.java
@@ -1,12 +1,10 @@
 package com.foo.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.foo.domain.UserVO;
