62f35bf2c89d7b0a0a4c77b5470b085b6c7f39e3
LingLing <ryoung6920@gmail.com>
2023-08-07 23:51:13 +0900

    Feat : 카카오 연동 로그인 - 사용자 정보 DB와 비교하여 로그인 및 회원가입 후 메인페이지로 이동

--- src/main/java/com/foo/controller/UserRestController.java
+++ src/main/java/com/foo/controller/UserRestController.java
@@ -1,5 +1,4 @@
 package com.foo.controller;
-import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.foo.service.UserService;
 
 
@@ -64,32 +61,9 @@ public class UserRestController {
 		
 		return result;
 	}
-	
-	
-	
-	// 2. 카카오 연동 로그인
-	@RequestMapping(value = "/kakaoLogin", method = RequestMethod.GET)
-	public ModelAndView kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Exception {
 
-		// 1번
-		// 카카오톡에 사용자 코드 받기 (jsp의 a태그 href에 경로 있음)
-		logger.debug("@@@@@@@@@@@@@@@ code : " + code);
-		
-		// 2번
-		// 받은 code를 service 메서드로 보내서 accessToken값 출력
-		String access_Token = uService.getAccessToken(code);
-		logger.debug("@@@@@@@@@@@@@@@ access_Token : " + access_Token);
-		
-		// 3번
-		// 받은 accessToken를 getUserInfo로 보내서 정보를 받아옴
-		// userInfo에 이름, 이메일 정보가 저장됨
-		HashMap<String, Object> userInfo = uService.getUserInfo(access_Token);
-		logger.debug("@@@@@@@@@@@@@@@ 이름 : " + userInfo.get("nickname"));
-		logger.debug("@@@@@@@@@@@@@@@ 이메일 : " + userInfo.get("email"));
-		
-		return null;	
-		// 여기서는 코드가 넘어오는지만 확인
-	}
+	
+	
 
 	
 	
